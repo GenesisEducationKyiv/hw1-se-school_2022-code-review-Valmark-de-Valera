f9699c6d045e2cac7841e48ae7f0a13221c233e6
Valerii Marchuk <marchukgwmb@gmail.com>
2022-08-28 11:59:28 +0300

    Ready for review 1.1

--- services/utils.js
+++ services/utils.js
@@ -1,9 +1,8 @@
 function validateEmail(email) {
-	return String(email)
-		.toLowerCase()
-		.match(
-			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-		);
+	let regex =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	let result = String(email).toLowerCase().match(regex);
+	return Boolean(result);
 }
 
 module.exports = { validateEmail };
